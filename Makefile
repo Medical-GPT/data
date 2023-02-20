a0ab986e19abbcd22e0a93496e64ca5fe446a385
Kristian Apostolov <kristian@Kristians-MacBook-Pro.local>
2023-02-20 15:10:02 +0000

    Add cleanup of special characters

--- Makefile
+++ Makefile
@@ -54,6 +54,12 @@ preprocess: ## Preprocess concatenated file (created if not present)
 	@echo "    [✓]"
 	@echo
 
-.PHONY: install uninstall clean dwonload concat preprocess help
+remove_special: ## Remove special characters from dataset
+	@echo "==> Cleaning up file..."
+	@venv/bin/python src/clean_special_characters.py
+	@echo "    [✓]"
+	@echo
+
+.PHONY: install uninstall clean dwonload concat preprocess remove_special help
 help: ## Shows available targets
 	@fgrep -h "## " $(MAKEFILE_LIST) | fgrep -v fgrep | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-13s\033[0m %s\n", $$1, $$2}'
---
+++ src/clean_special_characters.py
@@ -0,0 +1,23 @@
+from constants import PREPROCESSED_FILE, PREPROCESSED_FILE_FOLDER
+import os
+
+assert os.path.exists(PREPROCESSED_FILE), "Make sure you have preprocessed data"
+
+ALLOWED_CHARACTERS = "!\"#$%&'‘’()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]_`abcdefghijklmnopqrstuvwxyz{|}~ \n"
+
+
+def clean():
+    temp_file = f"{PREPROCESSED_FILE_FOLDER}/temp.txt"
+    # fmt: off
+    with \
+        open(PREPROCESSED_FILE, 'r') as in_file, \
+        open(temp_file, 'w') as out_file:
+    # fmt: on
+        for line in in_file:
+            if any(c not in ALLOWED_CHARACTERS for c in line):
+                continue
+            out_file.write(line)
+
+
+if __name__ == "__main__":
+    clean()
